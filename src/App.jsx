d5d7d5f72a0ad36ecf3f217ca5f262af09fac3f5
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2023-06-26 20:52:29 +1000

    feat: add logo, navbar and text on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,22 @@
-import "./App.css";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Home } from "./pages";
+import { Navbar } from "./components";
 
 function App() {
-  setTimeout(() => {
-    var canvas = document.querySelector("canvas"),
-      // Initialising the canvas
-      ctx = canvas.getContext("2d");
+  
 
-    // Setting the width and height of the canvas
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    // Setting up the letters
-    var letters =
-      "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ";
-    letters = letters.split("");
-
-    // Setting up the columns
-    var fontSize = 10,
-      columns = canvas.width / fontSize;
-
-    // Setting up the drops
-    var drops = [];
-    for (var i = 0; i < columns; i++) {
-      drops[i] = 1;
-    }
-
-    // Setting up the draw function
-    function draw() {
-      ctx.fillStyle = "rgba(0, 0, 0, .1)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      for (var i = 0; i < drops.length; i++) {
-        var text = letters[Math.floor(Math.random() * letters.length)];
-        ctx.fillStyle = "#FFF";
-        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-        drops[i]++;
-        if (drops[i] * fontSize > canvas.height && Math.random() > 0.95) {
-          drops[i] = 0;
-        }
-      }
-    }
-
-    // Loop the animation
-    setInterval(draw, 33);
-  }, 500);
-
-  return <canvas></canvas>;
+  return <>
+    <Navbar></Navbar>
+  <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          {/* <Route path="*" element={<NoMatch />} /> */}
+        </Route>
+      </Routes>
+      </>;
 }
 
 export default App;
