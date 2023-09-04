f8ceba4546767e365739a560e63f5ec7ccec785d
Ygor Carmo <ygor.carmo@gmail.com>
2023-09-04 15:08:42 +1000

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import "./assets/fonts/Oxygen/Oxygen-Regular.ttf";
 import "./assets/fonts/Oxygen_Mono/OxygenMono-Regular.ttf";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 );
